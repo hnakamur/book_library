fcda45818d361715d055b464752d1a944f27a1d2
Hiroaki Nakamura <hnakamur@gmail.com>
2013-08-03 16:11:20 +0900

    Add files generated by the command "rails new .".

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '2923149fb08f0b2040494ba7a16d18ac3810c5f7d92819b4781e7c3640fc9d251ef00954fe648a36cd62c24be5876cf66ef1a6d92430cf310d67e20a99144279'
